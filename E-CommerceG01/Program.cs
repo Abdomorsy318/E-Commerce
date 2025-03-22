491522a7b59bd4c657e6d09be0d22ac3c31031d9
Abdo Morsy <abdomorsy318@gmail.com>
2025-03-23 00:10:41 +0200

    Session01

--- E-CommerceG01/Program.cs
+++ E-CommerceG01/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using Presistance.Data;
+
 namespace E_CommerceG01
 {
     public class Program
@@ -13,7 +16,10 @@ namespace E_CommerceG01
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"));
+            });
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
