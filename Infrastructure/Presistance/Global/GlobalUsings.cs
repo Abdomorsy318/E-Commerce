d27aa0aef3c3609c5657cad9d375cbb821cd6c9e
Abdo Morsy <abdomorsy318@gmail.com>
2025-03-23 18:09:15 +0200

    .

--- Infrastructure/Presistance/Global/GlobalUsings.cs
+++ Infrastructure/Presistance/Global/GlobalUsings.cs
@@ -3,3 +3,6 @@ global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using System.Reflection;
 
+global using System.Collections.Concurrent;
+global using Domain.Contracts;
+global using Presistance.Data;
